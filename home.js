8cc68c13072f970f55375bdfedb121de9e4a4af0
Abdullahbdbd <abdullahmohammad262000@gmail.com>
2025-08-28 22:07:40 +0600

    call and clear section

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
+// const { createElement } = require("react");
+
 // heart button
 const hearts = document.getElementsByClassName('heart');
 for(const heart of hearts){
@@ -9,6 +11,82 @@ heart.addEventListener('click',
     }
 )   
 }
+
+
+// call section 
+const historyData = [];
+function callBtnClick(btnID, titalId, nameId, numId){
+    document.getElementById(btnID).addEventListener('click',
+    function(){
+        const serviceTital = document.getElementById(titalId).innerText;
+        const serviceName = document.getElementById(nameId).innerText;
+        const callNum = document.getElementById(numId).innerText;
+        const coinCount = parseInt(document.getElementById('coin-count').innerText);
+
+        const data = {
+            tital: serviceTital,
+            name: serviceName,
+            time: new Date().toLocaleTimeString()
+        }
+
+        if(coinCount < 20){
+            alert('❌ ' + "You don't have enough coins. You need at least 20 Coins to make the call.")
+        }
+        
+        else{
+            alert('📞 '+"Calling " + serviceName + ' ' + callNum) ;
+            const newCoinCount = coinCount - 20;
+            document.getElementById('coin-count').innerText = newCoinCount;
+
+            historyData.push(data);
+
+            const historyDataSection = document.getElementById('history-data-section');
+            historyDataSection.innerText = '';
+
+            for(const data of historyData){
+              const div = document.createElement("div");
+              div.innerHTML = `
+              <div
+             
+               class="flex justify-between items-center mt-5 bg-[#fafafa] p-2 rounded-lg"
+             >
+               <!-- history text  -->
+               <div>
+                 <h2 class="text-sm font-semibold max-w-[150px]">${data.tital}</h2>
+                 <p class="text-xs text-[#5c5c5c]">${data.name}</p>
+               </div>
+               <!-- history time  -->
+               <div>
+                 <p class="text-xs text-[#111111]">${data.time}</p>
+               </div>
+             </div>
+              `;
+
+              historyDataSection.appendChild(div);
+            }
+            
+            // return callNum, serviceName; 
+        }
+    }
+)
+}
+
+callBtnClick('N-E-CallBtn', 'N-E-STital', 'N-E-Sn', 'N-E-HL-Num')
+callBtnClick('P-H-CallBtn', 'P-H-STital', 'P-H-Sn', 'P-H-HL-Num')
+callBtnClick('F-S-CallBtn', 'F-S-STital', 'F-S-Sn', 'F-S-HL-Num')
+callBtnClick('A-S-CallBtn', 'A-S-STital', 'A-S-Sn', 'A-S-HL-Num')
+callBtnClick('W-C-CallBtn', 'W-C-STital', 'W-C-Sn', 'W-C-HL-Num')
+callBtnClick('A-C-CallBtn', 'A-C-STital', 'A-C-Sn', 'A-C-HL-Num')
+
+
+//clear section
+document.getElementById('clear-btn').addEventListener('click',
+    function(){
+        const historyDataSection = document.getElementById('history-data-section');
+        historyDataSection.innerHTML = '';
+    }
+)
+
 
 //challenge part
 
@@ -25,7 +103,6 @@ copyCountBtn.addEventListener('click',
 }
 
 // copy modal section 
-
 function copyBtnClick(btnID, numId){
     document.getElementById(btnID).addEventListener('click',
     function(){
